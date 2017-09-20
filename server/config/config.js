d26cbaddbbe74bce7d0ee54a0cf1642a09f1f2ea
Prakash Pudhucode <optimusprike@users.noreply.github.com>
2017-09-19 23:52:31 -0400

    Add files via upload

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+	uri: 'mongodb://pudhucodep:123@ds129024.mlab.com:29024/cen3031-assignment3', //place the URI of your mongo database here.
+  }, 
+  googleMaps: {
+    key: ''
+  },
+  port: 8080
+};
